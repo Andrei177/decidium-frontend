36f002b6a110093d41e4cfab5f84d74ec0f8cea7
Matvey <venz9@mail.ru>
2025-04-03 01:47:02 +0500

    страничка входа

--- src/pages/signin/ui/SignIn.tsx
+++ src/pages/signin/ui/SignIn.tsx
@@ -1,11 +1,27 @@
+import FormWrapper from "../../../shared/form-wrapper/FormWrapper"
+import Button from "../../../shared/ui/button/Button"
+import Input from "../../../shared/ui/input/Input"
+import s from "./SignIn.module.css"
+
 export const SignIn = () => {
     return (
-        <form>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" />
+        <FormWrapper>
+            <form className={s.dd}>
 
-            <label htmlFor="password">Password</label>
-            <input type="text" name="password" />
-        </form>
+                <label className={s.vxod}>Войти</label>
+                <label className={s.mail} htmlFor="email">Email</label>
+                <Input type="email" name="email" placeholder="Введите email..." />
+
+                <label className={s.password} htmlFor="password">Пароль</label>
+                <Input type="text" name="password" placeholder="Введите пароль..." />
+
+                <Button className={s.signInn}>Войти</Button>
+                <button className={s.zabpass}>Забыли пароль?</button>
+                <button className={s.passRec}>Еще нет аккаунта?</button>
+            </form>
+
+
+        </FormWrapper>
     )
 }
+
