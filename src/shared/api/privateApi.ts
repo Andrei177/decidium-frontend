150138fffaaabfa30613ce45aafdf01cf2b27acf
Andrey <andrejlapin4594@gmail.com>
2025-04-24 20:52:00 +0500

    feat: создал экземпляры axios для запросов на сервер и описал перехватчики запросов для обновления токенов

---
+++ src/shared/api/privateApi.ts
@@ -0,0 +1,49 @@
+import axios, { AxiosError } from "axios";
+import { API_URL } from "../config/backend";
+
+export const $privateApi = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+$privateApi.interceptors.request.use((req) => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    req.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return req;
+});
+
+$privateApi.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config;
+
+    // Проверяем, что это 401 ошибка и что запрос еще не повторялся
+    if (
+      error.response?.status === 401 &&
+      originalRequest &&
+      !(originalRequest as any)._retry
+    ) {
+      (originalRequest as any)._retry = true; // Помечаем запрос как повторный
+
+      try {
+        const refreshResponse = await $privateApi.post("/jwt/refresh");
+
+        if (refreshResponse.data.accessToken) {
+          localStorage.setItem("accessToken", refreshResponse.data.accessToken);
+        }
+
+        originalRequest.headers.Authorization = `Bearer ${localStorage.getItem("accessToken")}`;
+
+        return $privateApi(originalRequest);
+      } catch (refreshError) {
+        localStorage.removeItem("accessToken");
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
