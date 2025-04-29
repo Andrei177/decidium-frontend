3260c9760170e7d40d4a3ea41ada95903ef84eb5
Andrey <andrejlapin4594@gmail.com>
2025-04-29 22:24:02 +0500

    fix: убрал ненужную проверку из эффекта и переменную из стора

--- src/features/auth/model/auth-store/auth-store.ts
+++ src/features/auth/model/auth-store/auth-store.ts
@@ -3,13 +3,9 @@ import { create } from "zustand";
 interface IAuthStore{
     isAuth: boolean;
     setIsAuth: (bool: boolean) => void; 
-    hasRefreshed: boolean;
-    setHasRefreshed: (bool: boolean) => void; 
 }
 
 export const useAuthStore = create<IAuthStore>(set => ({
     isAuth: false,
-    setIsAuth: (bool) => set({isAuth: bool}),
-    hasRefreshed: false,
-    setHasRefreshed: (bool) => set({hasRefreshed: bool})
+    setIsAuth: (bool) => set({isAuth: bool})
 }))
--- src/features/auth/ui/AuthWrapper.tsx
+++ src/features/auth/ui/AuthWrapper.tsx
@@ -4,52 +4,40 @@ import { refreshTokens } from "../api/authApi";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { Loader, Routes } from "../../../shared";
 import s from "./AuthWrapper.module.css"
+import { isPublicPage } from "../lib/isPublicPage";
 
 export const AuthWrapper = () => {
 
-    const [isLoading, setIsLoading] = useState(false);
-    const { setIsAuth, hasRefreshed, setHasRefreshed } = useAuthStore();
+    const [isLoading, setIsLoading] = useState(true);
+    const { setIsAuth } = useAuthStore();
     const navigate = useNavigate();
     const { pathname } = useLocation();
 
     useEffect(() => {
-        if (!hasRefreshed) {
-            setIsLoading(true);
-            refreshTokens()
-                .then(res => {
-                    console.log("Ответ при обновлении токенов", res)
-                    setIsAuth(true);
-                    if (pathname == Routes.SIGNIN
-                        || pathname == Routes.SIGNUP_PREPOD
-                        || pathname == Routes.SIGNUP_STUD
-                        || pathname == Routes.ROOT) {
-                        navigate(Routes.PROFILE);
-                    }
-                })
-                .catch(err => {
-                    console.error("Ошибка при обновлении токенов", err)
-                    setIsAuth(false);
-                    if (!(pathname == Routes.SIGNIN
-                        || pathname == Routes.SIGNUP_PREPOD
-                        || pathname == Routes.SIGNUP_STUD
-                        || pathname == Routes.ROOT)) {
-                        navigate(Routes.SIGNIN);
-                    }
-                })
-                .finally(() => {
-                    setIsLoading(false)
-                    setHasRefreshed(true)
-                })
-        }
+        setIsLoading(true);
+        refreshTokens()
+            .then(res => {
+                console.log("Ответ при обновлении токенов", res)
+                setIsAuth(true);
+                if (isPublicPage(pathname)) {
+                    navigate(Routes.PROFILE);
+                }
+            })
+            .catch(err => {
+                console.error("Ошибка при обновлении токенов", err)
+                setIsAuth(false);
+                if (!isPublicPage(pathname)) {
+                    navigate(Routes.SIGNIN);
+                }
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
     }, [])
 
-    return (
-        <>
-            {
-                isLoading
-                    ? <div className={s.loader_wrap}><Loader/></div>
-                    : <Outlet/>
-            }
-        </>
-    )
+    if (isLoading) {
+        return <div className={s.loader_wrap}><Loader /></div>;
+    }
+
+    return <Outlet />;
 }
